9d5bf44fdf9968c353f10fff53ea643085a50649
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-08-26 17:35:14 +0530

    working on that

---
+++ app.js
@@ -0,0 +1,51 @@
+const form = document.forms[0]
+const siteTitle = document.getElementById('site-title')
+const siteLink = document.getElementById('site-link')
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const title = siteTitle.value;
+    const url = siteLink.value;
+
+    var bookmark = {
+        name: title,
+        siteUrl : url
+    }
+
+    if(localStorage.getItem('bookmarks') === null){
+        // Init array 
+        var bookmarks = []
+        bookmarks.push(bookmark)
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+    }else{
+        var getBookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+        getBookmarks.push(bookmark)
+
+        // re set to local storage
+        localStorage.setItem('bookmarks', JSON.stringify(getBookmarks))
+    }
+
+
+    document.forms[0].reset()
+
+    fetchTheLocalStorage()
+
+})
+
+function fetchTheLocalStorage(){
+    var fecthBookMarks = JSON.parse(localStorage.getItem('bookmarks'))
+    const showBookMarks = document.querySelector('.links')
+    var bookmark = ''
+    for(i=0; i<fecthBookMarks.length; i++){
+        bookmark += `
+        
+            <div class="bookmark-box">
+                <h1>${fecthBookMarks[i].name}</h1>
+                <a href="${fecthBookMarks[i].siteUrl}"><button>Visit</button></a> <button>Remove</button>
+            </div>
+
+        `
+        showBookMarks.innerHTML = bookmark;
+    }
+}
